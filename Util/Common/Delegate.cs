214ae270d37920133a6edb7d9a7159242b6c5940
15168440402 <30889371+15168440402@users.noreply.github.com>
2021-09-28 16:06:59 +0800

    Add files via upload

---
+++ Util/Common/Delegate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Util.Common
+{
+    public static class Delegate
+    {
+        public delegate bool Compare<TCompare>(TCompare left, TCompare right);
+    }
+}
